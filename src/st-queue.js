a358948355f32deb922c53d41332d46648ec843c
Evgenia Kurineva <kurineva85@mail.ru>
2021-10-12 16:08:47 +0300

    feat: solve st-queue

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js'); // каждый узел имеет значение и указатель на следующий узел.
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-module.exports = class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+// Т.к. очередь должна быть на основе linked list(связанного списка), то вид очереди:
+
+//    head
+//     |
+// { value: 1, next: <obj_2> } -> { value: 7, next: <obj_3> } -> { value: 2, next: null }
+
+//            или как-то так
+
+//    head
+//     |
+// { value: 1, next: { value: 7, next: { value: 2, next: null } } }
+
+module.exports = class Queue {
+  constructor() {
+    this.head = null;
+    this.size = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    // throw new NotImplementedError('Not implemented');
+
+    // добавить элемент в конец очереди
+    const newElement = new ListNode(value);
+    if (this.size === 0) {
+      // если очереди(здесь - связанного списка) не было элементов => новый элемент будет первым элементом очереди(здесь - связанного списка) и на него должен указывать head
+      this.head = newElement;
+    } else {
+      // надо пройти по всей очереди(здесь - связанному списку) и найти последний элемент - его ссылкой next станет новый элемент
+      let currentElement = this.head;
+      while (currentElement.next) {
+        currentElement = currentElement.next;
+      }
+      currentElement.next = newElement;
+    }
+
+    // увеличить размер очереди на 1 элемент
+    this.size++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+
+    // удалить элемент из начала очереди(здесь - связанного списка) и вернуть его значение, перевести указатель head на новый первый элемент
+    const deletedElement = this.head;
+    if (this.size > 0) {
+      const newFirstElement = deletedElement.next;
+      this.head = newFirstElement;
+    }
+    // уменьшить размер очереди на 1 элемент
+    this.size--;
+    return deletedElement.value;
   }
 
-}
+  getUnderlyingList() {
+    // throw new NotImplementedError('Not implemented');
+
+    // вернуть весь список
+    return this.head;
+  }
+};
